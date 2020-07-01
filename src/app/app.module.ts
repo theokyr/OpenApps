5153cc8eace2c162700dabd0e12f1a01f014a782
theokyr <theo.kyrr@gmail.com>
2020-07-01 20:17:43 +0300

    Moved view-angular to its own module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,11 @@ import {AppRoutingModule} from './shared-angular/routing/app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AngularLibItApiModule} from "./lib-angular/angular-lib-it-api.module";
-import {CategoriesViewAngularModule} from "./view-angular/categories/categories-view-angular.module";
-import {LayoutViewAngularModule} from "./view-angular/layout/layout-view-angular.module";
 import {FlexLayoutModule} from "@angular/flex-layout";
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {BaseItApiService} from "./lib-angular/api/base-it-api.service";
 import {environment} from "../environments/environment";
-import {BulletinBoardAngularModule} from "./view-angular/bulletin-board/bulletin-board-angular.module";
-import {AuthenticationViewAngularModule} from "./view-angular/auth/authentication-view-angular.module";
-import {UsersViewAngularModule} from "./view-angular/users/users-view-angular.module";
-import {FeaturesViewAngularModule} from "./view-angular/feature/features-view-angular.module";
+import {ViewAngularModule} from "./view-angular/view-angular.module";
 
 @NgModule({
   declarations: [
@@ -27,12 +22,7 @@ import {FeaturesViewAngularModule} from "./view-angular/feature/features-view-an
     FontAwesomeModule,
     FlexLayoutModule,
     AngularLibItApiModule,
-    AuthenticationViewAngularModule,
-    BulletinBoardAngularModule,
-    LayoutViewAngularModule,
-    CategoriesViewAngularModule,
-    UsersViewAngularModule,
-    FeaturesViewAngularModule
+    ViewAngularModule
   ],
   providers: [],
   bootstrap: [AppComponent]
