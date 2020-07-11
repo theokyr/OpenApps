9468b5fa4b1cac76d114b44775a93a36c72d4bce
theokyr <theo.kyrr@gmail.com>
2020-07-11 15:10:43 +0300

    Firebase function custom rewriting

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,10 +7,27 @@ const {info} = require("firebase-functions/lib/logger");
 const {error} = require("firebase-functions/lib/logger");
 admin.initializeApp();
 
+const ERROR_CODE_ROUTE_NOT_FOUND: number = 1000;
+
 const app = express();
+info(`Init`)
+const API_PREFIX = 'api';
 
 app.use(cors({origin: true}));
+
+// Rewrite Firebase hosting requests: /api/:path => /:path
+app.use((req: any, res: any, next: any) => {
+  info(`/api - Rewritan from ${req.url}`);
+  if (req.url.indexOf(`/${API_PREFIX}/`) === 0) {
+    req.url = req.url.substring(API_PREFIX.length + 1);
+    info(`/api - Rewrote to ${req.url}`);
+  }
+  info(`/api - Returning next ${req.url}`);
+  next();
+});
+
 app.post('/callback', (req: any, res: any) => {
+  info(`/callback - Init`);
   if (!req.query) {
     error(`Invalid query`)
     res.status(400);
@@ -18,8 +35,9 @@ app.post('/callback', (req: any, res: any) => {
       'error': "No code has been specified"
     });
   }
+  info(`/callback - Getting query code`);
   const code = !!req.query.code ? req.query.code : "";
-  info(`Client ID: ${functions.config().auth.client_id.value}`)
+  info(`Client ID: ${functions.config().auth.client_id.value}`);
   request({
     method: 'POST',
     url: 'https://login.it.teithe.gr/token',
@@ -35,7 +53,14 @@ app.post('/callback', (req: any, res: any) => {
   }).pipe(res);
 });
 
+app.get((req: any, res: any) => {
+  res.status(404).json({
+    error: 'Could not find route',
+    code: ERROR_CODE_ROUTE_NOT_FOUND
+  })
+})
+
 exports.api = functions
-  .region('europe-west1')
+  // .region('europe-west1')
   .https
   .onRequest(app);
